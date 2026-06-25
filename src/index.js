848615450a23f05c14470d5737e05dea5ea42324
sapanag401-123 <sapanag401@gmail.com>
2026-06-25 12:36:30 +0545

    add crud operation

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ app.get("/users",(req,res)=>{
 
 //get by id
 //user/100 => {id:100}       rout params
-//
 app.get("/users/:id",(req,res)=>{
     //res.send("<h1> all users</h1>");
     //req.params => {}  => {id:1}
@@ -53,8 +52,6 @@ app.get("/users/:id",(req,res)=>{
 
 
 
-
-
 //post/create
 app.post("/users",(req,res)=>{
    // res.send("<h1> users created </h1>");
@@ -69,16 +66,19 @@ app.post("/users",(req,res)=>{
    })
 });
 
+
    //update
    app.put("/users/:id",(req,res)=>{
     res.send("<h1> users updated </h1>");
 });
+
 
 //delete
    app.delete("/users/:id",(req,res)=>{
     res.send("<h1> users delete </h1>");
 });
    
+
 //crud products
 //get all products
 app.get("/products",(req,res)=>{
@@ -94,6 +94,32 @@ app.get("/products",(req,res)=>{
    })
 });
 
+//get by id
+//user/100 => {id:100}       rout params
+app.get("/products/:id",(req,res)=>{
+    //res.send("<h1> all users</h1>");
+    //req.params => {}  => {id:1}
+
+
+    const id = req.params.id;
+    res.json({
+    //message:`user by id 1 fetched`,
+    message:`products by id ${id} fetched`, //multiple ko lagi
+    success:true,
+    data:{
+        _id:1,
+        name:"sapana gurung",
+        email:"sapanagrg123@gmail.com",
+    },
+   })
+});
+
+
+
+
+
+
+
 //post
 app.post("/produncts",(req,res)=>{
     //res.send("<h1> products created </h1>");
@@ -108,13 +134,6 @@ app.post("/produncts",(req,res)=>{
    })
 });
 
-//get by id
-
-
-
-
-
-
    //update
    app.put("/products/:id",(req,res)=>{
     res.send("<h1> products updated </h1>");
@@ -124,10 +143,6 @@ app.post("/produncts",(req,res)=>{
    app.delete("/products/:id",(req,res)=>{
     res.send("<h1> products delete </h1>");
 });
-
-
-
-
 
 
 //ip -> 198.168.1.1:1112
